47f2c3ce6289c73787d512bc848d74f4a9353870
Kristiyan Kirov <kris15kirov@gmail.com>
2026-01-28 17:41:03 +0200

    Project finalization - Energy Efficiency Calculator complete

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+/**
+ * Energy Efficiency Calculator - Server
+ * Project finalized: January 2026
+ * © 2026 IICT-BAS
+ */
+
 const express = require('express');
 const path = require('path');
 const app = express();
